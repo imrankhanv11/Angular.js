8a9c3116bfafddaf87d82cb2152407d36c5b9526
Imran Khan V <146421206+imrankhanv11@users.noreply.github.com>
2025-11-17 15:06:35 +0530

    Add files via upload

---
+++ BookManagement/src/app/api/enpoints.js
@@ -0,0 +1,22 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('myApp')
+        .constant('Endpoints', {
+            BASE_URL: "http://localhost:5007/api",
+
+            BOOK: {
+                GET_ALL: "/Books/GellAllBooks",
+                CREATE: "/Books/AddBook",
+                UPDATE: "/Books/UpdateBook",
+                DELETE: "/Books/DeleteBook/"
+            },
+
+            USER: {
+                LOGIN: "/Login/LoginUser",
+                REGISTER: "/Login/RegisterUser"
+            }
+        });
+
+})();
